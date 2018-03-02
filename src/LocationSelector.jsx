b8753928559298bea9ad26f3f74d90d3745f59c4
youpy <youpy@buycheapviagraonlinenow.com>
2018-03-03 03:22:34 +0900

    add api key

--- src/LocationSelector.jsx
+++ src/LocationSelector.jsx
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Circle } from "react-google-map
 
 const LocationSelector = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBhy38F26IWNXwHX3M9lCz-ttxc-LFqyqI",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
