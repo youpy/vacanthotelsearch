54c4864e64815b3e338c296a06b61c05db7af091
youpy <youpy@buycheapviagraonlinenow.com>
2018-03-03 22:05:53 +0900

    add search box

--- src/LocationSelector.jsx
+++ src/LocationSelector.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { compose, withProps, withHandlers } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Circle } from "react-google-maps"
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
 
 const LocationSelector = compose(
   withProps({
@@ -12,12 +13,16 @@ const LocationSelector = compose(
   withHandlers(() => {
     const refs = {
       map: undefined,
-      latLng: undefined
+      latLng: undefined,
+      searchBox: undefined
     }
 
     return {
       onMapMounted: () => ref => {
         refs.map = ref
+      },
+      onSearchBoxMounted: () => ref => {
+        refs.searchBox = ref;
       },
       onClick: () => (event) => {
         refs.latLng = {
@@ -31,6 +36,17 @@ const LocationSelector = compose(
       },
       onRadiusChanged: ({ onLocationChange, center }) => (event) => {
         onLocationChange(center, +event.target.value);
+      },
+      onPlacesChanged: ({ onLocationChange, radius }) => (event) => {
+        const places = refs.searchBox.getPlaces();
+        const location = places[0].geometry.location;
+
+        refs.latLng = {
+          lat: location.lat(),
+          lng: location.lng()
+        };
+
+        onLocationChange(refs.latLng, radius)
       }
     }
   }),
@@ -51,6 +67,30 @@ const LocationSelector = compose(
           center={props.center}
           radius={props.radius}
         />
+        <SearchBox
+          ref={props.onSearchBoxMounted}
+          controlPosition={1}
+          onPlacesChanged={props.onPlacesChanged}
+        >
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+            style={{
+              background: '#fff',
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              marginTop: `10px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+            }}
+          />
+        </SearchBox>
       </GoogleMap>
       <div>
         <p className="input-field range-field">
